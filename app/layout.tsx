7734fbdf3ccd1d878e2069f5a8adc5bb1f451885
0xMatt <matthew.javelet@gmail.com>
2025-06-25 20:52:39 -0500

    Add vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { FlickeringGrid } from '@/components/magicui/flickering-grid';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -39,6 +41,8 @@ export default function RootLayout({
           maxOpacity={0.5}
           flickerChance={0.1}
         />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
